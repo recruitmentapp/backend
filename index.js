45bdc0bfe60497eacfb0f80c929b8fb019719e20
willwearing <willwearing@gmail.com>
2020-11-20 10:35:30 -0700

    adding stuff for heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./server.js");
 
 const PORT = process.env.PORT || 5000;
+const secret = process.env.SECRET_THING || "foo";
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`.random);
